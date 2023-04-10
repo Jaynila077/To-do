7809d81ceb21c214aedbb33b6f0c5cb665eb2d00
Jaynila077 <jaynila077@gmail.com>
2023-04-10 15:10:39 +0530

    API completed

--- index.js
+++ index.js
@@ -21,5 +21,23 @@ const Todo = require('./model/todo');
 app.get('/todos', async (req, res) => {
     const todos = await Todo.find();
     res.json(todos);
-})
+});
+app.post('/todos/new',  (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+    todo.save();
+    res.json(todo);
+});
+app.delete('/todos/delete/:id', async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id);
+    res.json(result);
+});
+app.put('/todos/complete/:id', async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+    todo.complete = !todo.complete;
+    todo.save();
+    res.json(todo);
+});
+
 app.listen(5000, () => console.log('Server started on port 5000'));     
