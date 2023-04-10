ce5a53ba3d121df5b754b84a92f73116345f96a2
Jaynila077 <jaynila077@gmail.com>
2023-04-10 14:43:05 +0530

    used MongoDB Atlas

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const DB = 'mongodb+srv://jaynila077:jasa077@cluster0.waajaau.mongodb.net/?retryWrites=true&w=majority';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/mern-todo',{
+mongoose.connect(DB ,{
     useNewurlParser: true,
     useUnifiedTopology: true
 })
@@ -22,4 +22,4 @@ app.get('/todos', async (req, res) => {
     const todos = await Todo.find();
     res.json(todos);
 })
-app.listen(5000, () => console.log('Server started on port 5000'));    
+app.listen(5000, () => console.log('Server started on port 5000'));     
